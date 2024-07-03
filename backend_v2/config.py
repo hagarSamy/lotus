80d8df0dd37dc544a445a72636ed2ba69aba9217
hagarSamy <hs1800@fayoum.edu.eg>
2024-07-03 12:42:28 +0300

    cleaned

--- backend_v2/config.py
+++
@@ -1,6 +0,0 @@
-import os
-class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    # determining which db to connect to
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///site.db'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
--- backend_v2/models/__init__.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python3
-from models.engine.db_storage import DBStorage
-
-# Create a singleton instance
-storage = DBStorage()
-storage.reload()
