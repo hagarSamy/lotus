0dd357147af182e0ce21edd07b4aeff130b20014
hagarSamy <hs1800@fayoum.edu.eg>
2024-07-03 12:40:57 +0300

    cleaned

--- backend/models/base.py
+++ backend/models/base.py
@@ -1,35 +1,3 @@
-# #!/usr/bin/python3
-# import uuid
-# from datetime import datetime
-# from sqlalchemy.ext.declarative import declarative_base
-# from sqlalchemy import Column, String, DateTime
-# # from models.engine import storage
-# Base = declarative_base()
-
-# class BaseModel(Base):
-#     '''The baseModel class'''
-#     __tablename__ = 'base_model'
-
-#     id = Column(String(60), primary_key=True, nullable=False, default=lambda: str(uuid.uuid4()))
-#     created_at = Column(DateTime, nullable=False, default=datetime.utcnow)
-#     updated_at = Column(DateTime, nullable=False, default=datetime.utcnow)
-
-#     def __init__(self, *args, **kwargs):
-#         """Initializes the base model"""
-#         super().__init__(*args, **kwargs)
-#         if 'id' not in kwargs:
-#             self.id = str(uuid.uuid4())
-#         if 'created_at' not in kwargs:
-#             self.created_at = datetime.utcnow()
-#         if 'updated_at' not in kwargs:
-#             self.updated_at = datetime.utcnow()
-
-#     def save(self):
-#         from models.engine import storage
-#         """Updates updated_at and saves the model"""
-#         self.updated_at = datetime.utcnow()
-#         storage.new(self)
-#         storage.save()
 #!/usr/bin/python3
 """
 Contains class BaseModel
@@ -70,16 +38,6 @@ class BaseModel:
                 self.updated_at = datetime.utcnow()
             if "id" not in kwargs:
                 self.id = str(uuid.uuid4())
-            # if kwargs.get("created_at", None) and type(self.created_at) is str:
-            #     self.created_at = datetime.strptime(kwargs["created_at"], time)
-            # else:
-            #     self.created_at = datetime.utcnow()
-            # if kwargs.get("updated_at", None) and type(self.updated_at) is str:
-            #     self.updated_at = datetime.strptime(kwargs["updated_at"], time)
-            # else:
-            #     self.updated_at = datetime.utcnow()
-            # if kwargs.get("id", None) is None:
-            #     self.id = str(uuid.uuid4())
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.utcnow()
